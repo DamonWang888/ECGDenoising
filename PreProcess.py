1c32b3795648239bc24cec401c4009fa2cb6df24
damonwang888 <1225228598@qq.com>
2020-07-29 19:03:20 +0800

    Initial commit

---
+++ PreProcess.py
@@ -0,0 +1,73 @@
+import wfdb
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+import ipdb
+plt.rcParams['figure.figsize']=(10,10)
+
+# load data from nstdb
+# *********noisy type**********
+# ma:muscle (EMG) artifact (in record 'ma')
+# bw:baseline wander (in record 'bw')
+# em:electrode motion artifact (in record 'em')
+# *****link**********
+# https://physionet.org/content/nstdb/1.0.0/
+def load_data(fileDir):
+    file_path_list = []
+    valid_file_extensions = [".dat"]
+    valid_file_extensions = [item.lower() for item in valid_file_extensions]
+
+    for file in os.listdir(fileDir):
+        extension = os.path.splitext(file)[1]
+        # print(extension)
+        if extension.lower() not in valid_file_extensions:
+            continue
+        file_path_list.append(os.path.join(fileDir, file))
+    print(len(file_path_list))
+    ECG = []
+    for path in file_path_list:
+        base=os.path.basename(path)
+        base = os.path.splitext(base)[0]
+        print(fileDir+'/%s'%(base))
+        sample = wfdb.rdsamp(fileDir+'/%s'%(base))
+        print(sample[1]['comments'])
+        ECG.append(sample[0][:,1])
+    ECG = np.asarray(ECG)
+    return ECG
+
+
+# load_data('/home/wcj/DataSet/physionet.org/files/nstdb/1.0.0')
+
+# original_data=wfdb.rdsamp('/home/wcj/DataSet/physionet.org/files/nstdb/1.0.0/ma')
+# print(original_data)
+# print(original_data[1]['comments'])
+# print(type(original_data[1]['comments']))
+# print(len(original_data[1]['comments']))
+# print(original_data[1]['comments'][0].split(' ')[-2])
+
+# show noisy data
+# em_data=wfdb.rdsamp('/home/wcj/DataSet/physionet.org/files/nstdb/1.0.0/em')
+# bw_data=wfdb.rdsamp('/home/wcj/DataSet/physionet.org/files/nstdb/1.0.0/bw')
+# ma_data=wfdb.rdsamp('/home/wcj/DataSet/physionet.org/files/nstdb/1.0.0/ma')
+# print("em_data:",em_data)
+# print("bw_data:",bw_data)
+# print("ma_data:",ma_data)
+# plt.plot(em_data[0][1:1000,1],label='em')
+# plt.plot(bw_data[0][1:1000,1],label='bw')
+# plt.plot(ma_data[0][1:1000,1],label='ma')
+# plt.legend()
+# plt.show()
+
+# read clean ecg
+data118=wfdb.rdsamp('/home/wcj/DataSet/physionet.org/files/mitdb/1.0.0/118')
+data119=wfdb.rdsamp('/home/wcj/DataSet/physionet.org/files/mitdb/1.0.0/119')
+print(data118)
+plt.plot(data118[0][1:1000,1],label='118')
+plt.plot(data119[0][1:1000,1],label='119')
+plt.legend()
+plt.show()
+
+
+
+
+
---
+++ model.py
@@ -0,0 +1,69 @@
+from tensorflow import  keras
+import  tensorflow as tf
+import numpy as np
+import tensorflow.keras.backend as K
+from tensorflow.keras.layers import Conv2DTranspose, Lambda
+
+
+# def Conv1DTranspose(input_tensor, filters, kernel_size, strides=2, padding='valid'):
+#     """
+#         input_tensor: tensor, with the shape (batch_size, time_steps, dims)
+#         filters: int, output dimension, i.e. the output tensor will have the shape of (batch_size, time_steps, filters)
+#         kernel_size: int, size of the convolution kernel
+#         strides: int, convolution step size
+#         padding: 'valid' | 'valid'
+#     """
+#     x = Lambda(lambda x: K.expand_dims(x, axis=2))(input_tensor)
+#     x = Conv2DTranspose(filters=filters, kernel_size=(kernel_size, 1), strides=(strides, 1), padding=padding)(x)
+#     x = Lambda(lambda x: K.squeeze(x, axis=2))(x)
+#     return x
+
+class Conv1DTranspose(tf.keras.layers.Layer):
+    def __init__(self, filters, kernel_size, strides=1, padding='valid',output_padding=None):
+        super().__init__()
+        self.conv2dtranspose = tf.keras.layers.Conv2DTranspose(
+          filters, (kernel_size, 1), (strides, 1), padding
+        )
+
+    def call(self, x):
+        x = tf.expand_dims(x, axis=2)
+        x = self.conv2dtranspose(x)
+        x = tf.squeeze(x, axis=2)
+        return x
+
+class ECGDAE:
+    def __init__(self,channel,siglength):
+        self.channel=channel
+        self.siglenth=siglength
+        self.inputshape=(siglength,channel)
+    def build(self):
+        model=keras.Sequential()
+        model.add(keras.layers.Conv1D(32,4,2,input_shape=self.inputshape))#filters,kernel_size,strides
+        model.add(keras.layers.PReLU())
+        model.add(keras.layers.Conv1D(64, 4, 2))  # filters,kernel_size,strides
+        model.add(keras.layers.PReLU())
+        model.add(keras.layers.Conv1D(128, 4, 2))  # filters,kernel_size,strides
+        model.add(keras.layers.PReLU())
+        model.add(keras.layers.Conv1D(256, 4, 2))  # filters,kernel_size,strides
+        model.add(keras.layers.PReLU())
+        model.add(keras.layers.Conv1D(512, 4, 2))  # filters,kernel_size,strides
+        model.add(keras.layers.PReLU())
+        model.add(Conv1DTranspose(256, 4, 2,padding='valid' ))  # filters,kernel_size,strides
+        model.add(keras.layers.PReLU())
+        model.add(Conv1DTranspose(128, 5, 2,padding='valid'))  # filters,kernel_size,strides
+        model.add(keras.layers.PReLU())
+
+        model.add(Conv1DTranspose(64, 4, 2,padding='valid'))  # filters,kernel_size,strides
+        model.add(keras.layers.PReLU())
+        model.add(Conv1DTranspose(32, 4, 2,padding='valid'))  # filters,kernel_size,strides
+        model.add(keras.layers.PReLU())
+        model.add(Conv1DTranspose(1, 6, 2,padding='valid'))  # filters,kernel_size,strides
+        model.add(keras.layers.PReLU())
+        return  model
+
+print(tf.__version__)
+ECGDAEModel=ECGDAE(1,1000).build()
+ECGDAEModel.summary()
+
+# InputData=np.random.rand(1,1000)
+# print(InputData.shape)
